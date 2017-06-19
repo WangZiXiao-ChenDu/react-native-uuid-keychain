b48941e016a7c1380451536d53a9faaf0fee37ef
Wang Zi Xiao <328249923@qq.com>
2017-06-19 10:06:30 +0800

    upDated RN-0.40

--- ios/RNGetUUID+KeyChain/RNGetUUID_KeyChain.h
+++ ios/RNGetUUID+KeyChain/RNGetUUID_KeyChain.h
@@ -6,8 +6,12 @@
 //  Copyright © 2017年 WangZiXiao. All rights reserved.
 //
 
-#import "RCTBridgeModule.h"
 #import <Foundation/Foundation.h>
+#if __has_include(<React/RCTBridgeModule.h>)
+#import <React/RCTBridgeModule.h>
+#else
+#import "RCTBridgeModule.h"
+#endif
 
 @interface RNGetUUID_KeyChain : NSObject<RCTBridgeModule>
 
